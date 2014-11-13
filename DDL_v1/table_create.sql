b13cf8bc0293aaabc95b4bb76a3f45241a1df963
Xinyi Ou <ouxinyi@users.noreply.github.com>
2014-11-13 15:26:22 -0500

    Update table_create.sql

--- DDL_v1/table_create.sql
+++ DDL_v1/table_create.sql
@@ -30,48 +30,48 @@ CREATE TABLE Passenger(
 	age INTEGER NOT NULL,
 	guardian INTEGER /*Passengers over 16 do not need a guardian*/
 );
-
+--
 CREATE TABLE Plane(
 	plane_id INTEGER PRIMARY KEY,
 	seating_capacity INTEGER NOT NULL,
 	carrying_weight DOUBLE PRECISION NOT NULL
 );
-
+--
 CREATE TABLE Maintained(
 	plane_id INTEGER,
 	service_date TIMESTAMP,
 	essn INTEGER,
 	PRIMARY KEY(plane_id, service_date, essn)
 );
-
+--
 CREATE TABLE Flight(
 	fid INTEGER PRIMARY KEY,
 	origin CHAR(40),
 	destination CHAR(40),
-	ETD TIMESTAMP WITH LOCAL TIME STAMP,
+	ETD TIMESTAMP,
 	duration INTEGER,
 	gate CHAR(10),
 	plane_id INTEGER
 );
-
+--
 CREATE TABLE Employee(
 	essn INTEGER PRIMARY KEY,
 	name CHAR(20) NOT NULL,
 	job_title CHAR(20) NOT NULL
 );
-
+--
 CREATE TABLE Certifications(
 	essn INTEGER,
 	certificate CHAR(20),
 	PRIMARY KEY(essn, certificate)
 );
-
+--
 CREATE TABLE Works_On(
 	essn INTEGER,
 	fid INTEGER,
 	PRIMARY KEY(essn, fid)
 );
-
+--
 CREATE TABLE Passenger_Flight_Info(
 	passenger_id INTEGER,
 	fid INTEGER,
@@ -79,7 +79,7 @@ CREATE TABLE Passenger_Flight_Info(
 	pieces_of_luggage INTEGER,
 	PRIMARY KEY(passenger_id, fid, seat_number)
 );
-
+--
 CREATE TABLE Seats_On_Flight(
 	fid INTEGER,
 	seat_number INTEGER,
@@ -95,7 +95,7 @@ SET FEEDBACK OFF
 ALTER TABLE passenger
 ADD FOREIGN KEY (guardian) references passenger(passenger_id)
 Deferrable initially deferred;
-
+--
 -- -----------------------------------------------------------------------------
 -- Populate the database instance
 -- -----------------------------------------------------------------------------
@@ -128,19 +128,19 @@ SELECT * FROM Seats_On_Flight;
 --2. A comment line stating the query in English. 
 --3. The SQL code for the query. 
 -- 
-
+--
 /* (Q1) - self-join
  Find the passenger_id, name, and age of every passenger that has a guardian.
 */
 -- <<<SQL CODE GOES HERE>>>
 --
-
+--
 --< The insert/delete/update statements to test the enforcement of ICs > 
 --Include the following items for every IC that you test (Important: see the next section titled 
 --“Submit a final report” regarding which ICs to test). 
 --A comment line stating: Testing: < IC name> 
 --A SQL INSERT, DELETE, or UPDATE that will test the IC.
-
+--
 COMMIT 
 -- 
 SET ECHO OFF
